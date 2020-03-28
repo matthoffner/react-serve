31d6d341fe30ca3ddc983338671864e536acc68f
Matt Hoffner <matthoffner@Matts-MacBook-Air.local>
2020-03-28 13:06:37 -0700

    create files if not existing

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # reactmon
 
 Bare minimum example of using react-refresh for the latest in hot reloading. Basically a copy of `rollup-plugin-react-refresh` example at this point.
+
+## usage
+
+Can create a src and public directory:
+
+```js
+npm install reactmon --save-dev
+```
+
+Then run:
+
+```js
+npx reactmon
+```
+
+Will create a `src` and `public` directory with basic entrypoint for getting started.
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 #! /usr/bin/env node
 const execa = require('execa');
-
+const fs = require('fs')
 async function reactmon() {
     try {
+        if (!fs.existsSync('src')) {
+            const srcPath = `${process.cwd()}/node_modules/reactmon/src/`;
+            const srcPathNew = `${process.cwd()}/src/`;
+            await execa('cp', ['-r', srcPath, srcPathNew]);
+        }
+        if (!fs.existsSync('public')) {
+            const htmlPath = `${process.cwd()}/node_modules/reactmon/public/`;
+            const htmlPathNew = `${process.cwd()}/public/`;
+            await execa('cp', ['-r', htmlPath, htmlPathNew]);
+        }
         const path = `${process.cwd()}/node_modules/.bin/nollup`;
         const configPath = `${process.cwd()}/node_modules/reactmon/rollup.config.js`;
         const contentBase = `${process.cwd()}/public`;
